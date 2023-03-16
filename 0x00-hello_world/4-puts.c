e8020018191344f9995af20121395755fbc7b517
Mohammed el faqyh <simofinance22@gmail.com>
2023-03-16 09:12:21 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
-*main -entry poit
-*return: always 0 (success)
-*/
+ * main -entry poit
+ * return: always 0 (succes)
+ */
 int main(void)
 {
-    puts("/"Programming is like building a multilingual puzzle");
-    return (0);
-}
-
+	puts("\"Programming is like building a multilingual puzzle");}
