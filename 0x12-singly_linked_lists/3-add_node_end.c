addaa1839307d14d23463f5d48eec10b8713f4ac
Mohammed el faqyh <simofinance22@gmail.com>
2023-04-27 16:42:18 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,6 +15,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *last_node = *head;
 
 	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
