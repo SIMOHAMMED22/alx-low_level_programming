53a6e17edf42bd75262b1dd8b3c141ae39427399
Mohammed el faqyh <simofinance22@gmail.com>
2023-04-27 16:23:28 -0700

    add list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,9 +14,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	if (!head || !str)
+	{
+		return (NULL);
+	}
+
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
+	{
 		return (NULL);
+	}
 
 	new_node->str = strdup(str);
 	if (!new_node->str)
@@ -25,6 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
