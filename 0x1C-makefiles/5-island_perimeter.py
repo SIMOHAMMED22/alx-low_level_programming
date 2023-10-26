aedc07e14f07081627a634088b934a1ebc8c2756
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-26 13:55:22 +0100

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,7 +25,7 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                # Check for adjacent land or boundaries and increment the perimeter
+
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
                 if row == rows - 1 or grid[row + 1][col] == 0:
